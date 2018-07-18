5e7272c9f360edc8ca5838788484b636e350acdb
DESKTOP-PLMC83U\User <dj.snowman11@gmail.com>
2018-07-18 23:44:05 +0800

    CMD,data共用狀態機

--- TOP.v
+++ TOP.v
@@ -4,13 +4,14 @@ module TOP(
 	inout sda
 );
 
-wire ctrl_d,sda_w,ctrl_l,ctrl_h;
+wire ctrl_d,sda_w,ctrl_l,ctrl_h,select;
 wire [6:0]cmd_address;
-wire [9:0]lcd_address;
-wire [7:0]cmd_data,lcd_data;
+wire [9:0]lcd_address,address;
+wire [7:0]cmd_data,lcd_data,data;
 
 assign sda = (ctrl_d)? sda_w : 1'bz;
 assign scl = ctrl_h | (~ctrl_l & ck1);
+assign data = (select)? lcd_data : cmd_data;
 
 divider ic1(
 	.ck(ck),
@@ -25,25 +26,24 @@ ctrl ic2(
 	.clk2(ck2),
 	.clk1(ck1),
 	.reset(reset),
-	.cmd_data(cmd_data),
-	.cmd_address(cmd_address),
 	.sda_w(sda_w),
 	.ctrl_d(ctrl_d),
 	.ctrl_l(ctrl_l),
 	.ctrl_h(ctrl_h),
-	.lcd_data(lcd_data),
-	.lcd_address(lcd_address)
+	.data(data),
+	.address(address),
+	.select(select)
 );
 
 cmd_rom ic3(
 	//.ck(ck),
-	.address(cmd_address),
+	.address(address),
 	.data_out(cmd_data)
 );
 
 lcd_rom ic4(
 	//.ck(ck),
-	.address(lcd_address),
+	.address(address),
 	.data_out(lcd_data)
 );
 
--- ctrl.v
+++ ctrl.v
@@ -1,17 +1,15 @@
 module ctrl(
 	input reset,clk2,sda,scl,clk1,
-	input [7:0]cmd_data,lcd_data,
-	output reg [6:0]cmd_address,
-	output reg [9:0]lcd_address,
-	output reg sda_w,ctrl_d,ctrl_l,ctrl_h
+	input [7:0]data,
+	output reg [9:0]address,
+	output reg sda_w,ctrl_d,ctrl_l,ctrl_h,select
 );
 
-reg [6:0]cmd_address_next;
-reg [9:0]lcd_address_next;
-reg [4:0]fsm,fsm_next;
+reg [9:0]address_next;
+reg [3:0]fsm,fsm_next;
 reg [3:0]add_con,add_con_next;
 reg [7:0]address_7a,cmd_mod_c0;
-reg sda_r;
+reg sda_r,select_next;
 
 always@(negedge clk2)
 begin
@@ -23,53 +21,53 @@ begin
 	if(~reset)
 	begin
 		fsm <= 4'd0;
-		cmd_address <= 7'd0;
 		add_con <= 4'd7;
-		address_7a <= 8'b01111010;
+		address_7a <= 8'h78;
 		cmd_mod_c0 <= 8'hc0;
-		lcd_address <= 10'd0;
+		address <= 10'd0;
+		select <= 1'b0;
 	end
 	else
 	begin
 		fsm <= fsm_next;
-		cmd_address <= cmd_address_next;
+		address <= address_next;
 		add_con <= add_con_next;
-		lcd_address <= lcd_address_next;
+		select <= select_next;
 	end
 end
 
 always@(*)
 begin
 	case(fsm)
-		5'd0:		//reset
+		4'd0:		//reset
 		begin
 			ctrl_d = 1'b1;
 			sda_w = 1'b1;
 			ctrl_h = 1'b1;
 			ctrl_l = 1'b0;
-			fsm_next = 5'd1;
-			cmd_address_next = cmd_address;
+			fsm_next = 4'd1;
 			add_con_next = add_con;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd1:		//start
+		4'd1:		//start
 		begin
 			ctrl_d = 1'b1;
 			sda_w = 1'b0;
 			ctrl_h = 1'b1;
 			ctrl_l = 1'b1;
-			fsm_next = 5'd2;
-			cmd_address_next = cmd_address;
+			fsm_next = 4'd2;
 			add_con_next = add_con;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd2:		//address => 7a
+		4'd2:		//address => 7a
 		begin
 			ctrl_d = 1'b1;
 			if(add_con == 4'd0)
 			begin
 				add_con_next = 4'd7;
-				fsm_next = 5'd3;
+				fsm_next = 4'd3;
 			end
 			else
 			begin
@@ -79,294 +77,154 @@ begin
 			sda_w = address_7a[add_con];
 			ctrl_h = 1'b0;
 			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd3:		//ack    _address => 7a
+		4'd3:		//ack    _address => 7a
 		begin
 			ctrl_d = 1'b0;
 			if(sda_r == 1'b1)
 			begin
-				fsm_next = 5'd0;
+				fsm_next = 4'd0;
 			end
 			else
 			begin
-				fsm_next = 5'd4;
+				fsm_next = 4'd4;
 			end
 			add_con_next = add_con;
 			sda_w = 1'b0;
 			ctrl_h = 1'b0;
 			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd4:		//cmd_mode  00
+		4'd4:		//cmd_mode  00
 		begin
 			ctrl_d = 1'b1;
 			if(add_con == 4'd0)
 			begin
 				add_con_next = 4'd7;
-				fsm_next = 5'd5;
+				fsm_next = 4'd5;
 			end
 			else
 			begin
 				add_con_next = add_con - 4'd1;
 				fsm_next = fsm;
 			end
-			sda_w = 1'b0;
+			if(select == 1'b0)
+			begin
+				sda_w = 1'b0;
+			end
+			else
+			begin
+				sda_w = cmd_mod_c0[add_con];
+			end
 			ctrl_h = 1'b0;
 			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd5:		//ack    _cmd_mode
+		4'd5:		//ack    _cmd_mode
 		begin
 			ctrl_d = 1'b0;
 			if(sda_r == 1'b1)
 			begin
-				fsm_next = 5'd0;
+				fsm_next = 4'd0;
 			end
 			else
 			begin
-				fsm_next = 5'd6;
+				fsm_next = 4'd6;
 			end
 			add_con_next = add_con;
 			sda_w = 1'b0;
 			ctrl_h = 1'b0;
 			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd6:		//cmd_data
+		4'd6:		//cmd_data
 		begin
 			ctrl_d = 1'b1;
 			if(add_con == 4'd0)
 			begin
 				add_con_next = 4'd7;
-				fsm_next = 5'd7;
+				fsm_next = 4'd7;
 			end
 			else
 			begin
 				add_con_next = add_con - 4'd1;
 				fsm_next = fsm;
 			end
-			sda_w = cmd_data[add_con];
+			sda_w = data[add_con];
 			ctrl_h = 1'b0;
 			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd7:		//ack     _cmd
+		4'd7:		//ack     _cmd
 		begin
 			ctrl_d = 1'b0;
 			if(sda_r == 1'b1)
 			begin
-				fsm_next = 5'd0;
-				cmd_address_next = cmd_address;
+				fsm_next = 4'd0;
+				address_next = address;
 			end
 			else
 			begin
-				cmd_address_next = cmd_address + 7'd1;
-				fsm_next = 5'd8;
+				address_next = address + 10'd1;
+				fsm_next = 4'd8;
 			end
 			add_con_next = add_con;
 			sda_w = 1'b0;
 			ctrl_h = 1'b0;
 			ctrl_l = 1'b0;
-			lcd_address_next = lcd_address;
+			select_next = select;
 		end
-		5'd8:		//stop1
+		4'd8:		//stop1
 		begin
 			ctrl_d = 1'b1;
 			sda_w = 1'b0;
 			ctrl_h = 1'b1;
 			ctrl_l = 1'b0;
-			fsm_next = 5'd9;
-			cmd_address_next = cmd_address;
+			fsm_next = 4'd9;
 			add_con_next = add_con;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
-		5'd9:		//stop2
+		4'd9:		//stop2
 		begin
 			ctrl_d = 1'b1;
 			sda_w = 1'b1;
 			ctrl_h = 1'b1;
 			ctrl_l = 1'b0;
-			if(cmd_address <= 7'd40)
+			if(select == 1'b0)
 			begin
-				fsm_next = 5'd1;
+				fsm_next = 4'd1;
+				if(address != 10'd40)
+				begin
+					select_next = 1'b0;
+					address_next = address;
+				end
+				else
+				begin
+					select_next = 1'b1;
+					address_next = 10'd0;
+				end
 			end
 			else
 			begin
-				fsm_next = 5'd10;
-			end
-			cmd_address_next = cmd_address;
-			add_con_next = add_con;
-			lcd_address_next = lcd_address;
-		end
-		//---------------------------------------------------------------------------------//
-		5'd10:		//reset
-		begin
-			ctrl_d = 1'b1;
-			sda_w = 1'b1;
-			ctrl_h = 1'b1;
-			ctrl_l = 1'b0;
-			fsm_next = 5'd11;
-			cmd_address_next = cmd_address;
-			add_con_next = add_con;
-			lcd_address_next = lcd_address;
-		end
-		5'd11:		//start
-		begin
-			ctrl_d = 1'b1;
-			sda_w = 1'b0;
-			ctrl_h = 1'b1;
-			ctrl_l = 1'b1;
-			fsm_next = 5'd12;
-			cmd_address_next = cmd_address;
-			add_con_next = add_con;
-			lcd_address_next <= lcd_address;
-		end
-		5'd12:		//address => 7a
-		begin
-			ctrl_d = 1'b1;
-			if(add_con == 4'd0)
-			begin
-				add_con_next = 4'd7;
-				fsm_next = 5'd13;
-			end
-			else
-			begin
-				add_con_next = add_con - 4'd1;
-				fsm_next = fsm;
-			end
-			sda_w = address_7a[add_con];
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
-		end
-		5'd13:		//ack    _address => 7a
-		begin
-			ctrl_d = 1'b0;
-			if(sda_r == 1'b1)
-			begin
-				fsm_next = 5'd10;
-			end
-			else
-			begin
-				fsm_next = 5'd14;
+				select_next = 1'b1;
+				address_next = address;
+				if(address != 10'd1023)
+				begin
+					fsm_next = 4'd1;
+				end
+				else
+				begin
+					fsm_next = 4'd10;
+				end
 			end
 			add_con_next = add_con;
-			sda_w = 1'b0;
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
-		end
-		5'd14:		//cmd_mode  C0
-		begin
-			ctrl_d = 1'b1;
-			if(add_con == 4'd0)
-			begin
-				add_con_next = 4'd7;
-				fsm_next = 5'd15;
-			end
-			else
-			begin
-				add_con_next = add_con - 4'd1;
-				fsm_next = fsm;
-			end
-			sda_w = cmd_mod_c0[add_con];
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
-		end
-		5'd15:		//ack    _cmd_mode
-		begin
-			ctrl_d = 1'b0;
-			if(sda_r == 1'b1)
-			begin
-				fsm_next = 5'd10;
-			end
-			else
-			begin
-				fsm_next = 5'd16;
-			end
-			add_con_next = add_con;
-			sda_w = 1'b0;
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
-		end
-		5'd16:		//LCD_data
-		begin
-			ctrl_d = 1'b1;
-			if(add_con == 4'd0)
-			begin
-				add_con_next = 4'd7;
-				fsm_next = 5'd17;
-			end
-			else
-			begin
-				add_con_next = add_con - 4'd1;
-				fsm_next = fsm;
-			end
-			sda_w = lcd_data[add_con];
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-			lcd_address_next = lcd_address;
-		end
-		5'd17:		//ack     LCD
-		begin
-			ctrl_d = 1'b0;
-			if(sda_r == 1'b1)
-			begin
-				fsm_next = 5'd10;
-				lcd_address_next <= lcd_address;
-			end
-			else
-			begin
-				lcd_address_next <= lcd_address + 10'd1;
-				fsm_next = 5'd18;
-			end
-			add_con_next = add_con;
-			sda_w = 1'b0;
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			cmd_address_next = cmd_address;
-		end
-		5'd18:		//stop1
-		begin
-			ctrl_d = 1'b1;
-			sda_w = 1'b0;
-			ctrl_h = 1'b1;
-			ctrl_l = 1'b0;
-			fsm_next = 5'd19;
-			cmd_address_next = cmd_address;
-			add_con_next = add_con;
-			lcd_address_next = lcd_address;
-		end
-		5'd19:		//stop2
-		begin
-			ctrl_d = 1'b1;
-			sda_w = 1'b1;
-			ctrl_h = 1'b1;
-			ctrl_l = 1'b0;
-			if(lcd_address <= 10'd1023)
-			begin
-				fsm_next = 5'd11;
-			end
-			else
-			begin
-				fsm_next = fsm;
-			end
-			cmd_address_next = cmd_address;
-			add_con_next = add_con;
-			lcd_address_next = lcd_address;
 		end
 		default:
 		begin
@@ -375,9 +233,9 @@ begin
 			ctrl_h = 1'b0;
 			ctrl_l = 1'b0;
 			fsm_next = fsm;
-			cmd_address_next = cmd_address;
 			add_con_next = add_con;
-			lcd_address_next = lcd_address;
+			address_next = address;
+			select_next = select;
 		end
 	endcase
 end

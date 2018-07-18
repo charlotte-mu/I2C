05615613319c894466b4793786631b8b14aac181
慕 <dj.snowman11@gmail.com>
2018-07-19 00:46:35 +0800

    狀態機簡化，將送data與ACK合併

--- TOP.v
+++ TOP.v
@@ -4,13 +4,13 @@ module TOP(
 	inout sda
 );
 
-wire ctrl_d,sda_w,ctrl_l,ctrl_h,select;
-wire [6:0]cmd_address;
-wire [9:0]lcd_address,address;
+wire scl_w,sda_w,ctrl_h,select;
+wire [9:0]address;
 wire [7:0]cmd_data,lcd_data,data;
 
-assign sda = (ctrl_d)? sda_w : 1'bz;
-assign scl = ctrl_h | (~ctrl_l & ck1);
+assign sda = (sda_w)? 1'bz : 1'b0;
+assign scl = (scl_w)? 1'bz : 1'b0;
+assign scl_w =(ctrl_h | ck1);
 assign data = (select)? lcd_data : cmd_data;
 
 divider ic1(
@@ -22,13 +22,10 @@ divider ic1(
 
 ctrl ic2(
 	.sda(sda),
-	.scl(scl),
 	.clk2(ck2),
 	.clk1(ck1),
 	.reset(reset),
 	.sda_w(sda_w),
-	.ctrl_d(ctrl_d),
-	.ctrl_l(ctrl_l),
 	.ctrl_h(ctrl_h),
 	.data(data),
 	.address(address),
--- ctrl.v
+++ ctrl.v
@@ -1,19 +1,32 @@
 module ctrl(
-	input reset,clk2,sda,scl,clk1,
+	input reset,clk2,sda,clk1,
 	input [7:0]data,
 	output reg [9:0]address,
-	output reg sda_w,ctrl_d,ctrl_l,ctrl_h,select
+	output reg sda_w,ctrl_h,select
 );
 
 reg [9:0]address_next;
-reg [3:0]fsm,fsm_next;
+reg [2:0]fsm,fsm_next;
 reg [3:0]add_con,add_con_next;
-reg [7:0]address_7a,cmd_mod_c0;
+reg [7:0]address_7a,cmd_mod;
+reg [1:0]mode,mode_next;
 reg sda_r,select_next;
 
 always@(negedge clk2)
 begin
 	sda_r <= sda;
+end
+
+always@(select)
+begin
+	if(select == 1'b0)
+	begin
+		cmd_mod = 8'h00;
+	end
+	else
+	begin
+		cmd_mod = 8'hc0;
+	end
 end
 
 always@(posedge clk2,negedge reset)
@@ -23,9 +36,9 @@ begin
 		fsm <= 4'd0;
 		add_con <= 4'd7;
 		address_7a <= 8'h78;
-		cmd_mod_c0 <= 8'hc0;
 		address <= 10'd0;
 		select <= 1'b0;
+		mode <= 2'd0;
 	end
 	else
 	begin
@@ -33,176 +46,116 @@ begin
 		address <= address_next;
 		add_con <= add_con_next;
 		select <= select_next;
+		mode <= mode_next;
 	end
 end
 
 always@(*)
 begin
 	case(fsm)
-		4'd0:		//reset
+		3'd0:		//reset
 		begin
-			ctrl_d = 1'b1;
 			sda_w = 1'b1;
 			ctrl_h = 1'b1;
-			ctrl_l = 1'b0;
-			fsm_next = 4'd1;
+			fsm_next = 3'd1;
 			add_con_next = add_con;
 			address_next = address;
 			select_next = select;
+			mode_next = mode;
 		end
-		4'd1:		//start
+		3'd1:		//start
 		begin
-			ctrl_d = 1'b1;
 			sda_w = 1'b0;
 			ctrl_h = 1'b1;
-			ctrl_l = 1'b1;
-			fsm_next = 4'd2;
+			fsm_next = 3'd2;
 			add_con_next = add_con;
 			address_next = address;
 			select_next = select;
+			mode_next = mode;
 		end
-		4'd2:		//address => 7a
+		3'd2:		//8bit
 		begin
-			ctrl_d = 1'b1;
+			ctrl_h = 1'b0;
 			if(add_con == 4'd0)
 			begin
 				add_con_next = 4'd7;
-				fsm_next = 4'd3;
+				fsm_next = 3'd3;
 			end
 			else
 			begin
 				add_con_next = add_con - 4'd1;
 				fsm_next = fsm;
 			end
-			sda_w = address_7a[add_con];
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
+			case(mode)
+				2'd0:
+				begin
+					sda_w = address_7a[add_con];
+				end
+				2'd1:
+				begin
+					sda_w = cmd_mod[add_con];
+				end
+				2'd2:
+				begin
+					sda_w = data[add_con];
+				end
+				default:
+				begin
+					sda_w = 1'b1;
+				end
+			endcase
 			address_next = address;
 			select_next = select;
+			mode_next = mode;
 		end
-		4'd3:		//ack    _address => 7a
+		3'd3:		//ack
 		begin
-			ctrl_d = 1'b0;
+			ctrl_h = 1'b0;
+			sda_w = 1'b1;
 			if(sda_r == 1'b1)
 			begin
-				fsm_next = 4'd0;
-			end
-			else
-			begin
-				fsm_next = 4'd4;
-			end
-			add_con_next = add_con;
-			sda_w = 1'b0;
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			address_next = address;
-			select_next = select;
-		end
-		4'd4:		//cmd_mode  00
-		begin
-			ctrl_d = 1'b1;
-			if(add_con == 4'd0)
-			begin
-				add_con_next = 4'd7;
-				fsm_next = 4'd5;
-			end
-			else
-			begin
-				add_con_next = add_con - 4'd1;
-				fsm_next = fsm;
-			end
-			if(select == 1'b0)
-			begin
-				sda_w = 1'b0;
-			end
-			else
-			begin
-				sda_w = cmd_mod_c0[add_con];
-			end
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			address_next = address;
-			select_next = select;
-		end
-		4'd5:		//ack    _cmd_mode
-		begin
-			ctrl_d = 1'b0;
-			if(sda_r == 1'b1)
-			begin
-				fsm_next = 4'd0;
-			end
-			else
-			begin
-				fsm_next = 4'd6;
-			end
-			add_con_next = add_con;
-			sda_w = 1'b0;
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			address_next = address;
-			select_next = select;
-		end
-		4'd6:		//cmd_data
-		begin
-			ctrl_d = 1'b1;
-			if(add_con == 4'd0)
-			begin
-				add_con_next = 4'd7;
-				fsm_next = 4'd7;
-			end
-			else
-			begin
-				add_con_next = add_con - 4'd1;
-				fsm_next = fsm;
-			end
-			sda_w = data[add_con];
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
-			address_next = address;
-			select_next = select;
-		end
-		4'd7:		//ack     _cmd
-		begin
-			ctrl_d = 1'b0;
-			if(sda_r == 1'b1)
-			begin
-				fsm_next = 4'd0;
+				fsm_next = 3'd0;
+				mode_next = 2'd0;
 				address_next = address;
 			end
 			else
 			begin
-				address_next = address + 10'd1;
-				fsm_next = 4'd8;
+				if(mode == 2'd2)
+				begin
+					fsm_next = 3'd4;
+					mode_next = 2'd0;
+					address_next = address + 10'd1;
+				end
+				else
+				begin
+					fsm_next = 3'd2;
+					mode_next = mode + 2'd1;
+					address_next = address;
+				end
 			end
 			add_con_next = add_con;
-			sda_w = 1'b0;
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
 			select_next = select;
 		end
-		4'd8:		//stop1
+		3'd4:		//stop1
 		begin
-			ctrl_d = 1'b1;
 			sda_w = 1'b0;
 			ctrl_h = 1'b1;
-			ctrl_l = 1'b0;
-			fsm_next = 4'd9;
+			fsm_next = 3'd5;
 			add_con_next = add_con;
 			address_next = address;
 			select_next = select;
+			mode_next = mode;
 		end
-		4'd9:		//stop2
+		3'd5:		//stop2
 		begin
-			ctrl_d = 1'b1;
 			sda_w = 1'b1;
 			ctrl_h = 1'b1;
-			ctrl_l = 1'b0;
 			if(select == 1'b0)
 			begin
-				fsm_next = 4'd1;
+				fsm_next = 3'd1;
 				if(address != 10'd40)
 				begin
-					select_next = 1'b0;
+					select_next = select;
 					address_next = address;
 				end
 				else
@@ -213,29 +166,29 @@ begin
 			end
 			else
 			begin
-				select_next = 1'b1;
+				select_next = select;
 				address_next = address;
 				if(address != 10'd1023)
 				begin
-					fsm_next = 4'd1;
+					fsm_next = 3'd1;
 				end
 				else
 				begin
-					fsm_next = 4'd10;
+					fsm_next = 3'd6;
 				end
 			end
 			add_con_next = add_con;
+			mode_next = mode;
 		end
 		default:
 		begin
-			ctrl_d = 1'b0;
-			sda_w = 1'b0;
-			ctrl_h = 1'b0;
-			ctrl_l = 1'b0;
+			sda_w = 1'b1;
+			ctrl_h = 1'b1;
 			fsm_next = fsm;
 			add_con_next = add_con;
 			address_next = address;
 			select_next = select;
+			mode_next = mode;
 		end
 	endcase
 end
